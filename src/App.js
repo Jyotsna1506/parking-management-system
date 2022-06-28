65641da70387cd7860717661432b8c6425ee3875
Jyotsna O.P <82457471+Jyotsna1506@users.noreply.github.com>
2022-06-28 16:05:16 +0530

    Add File Via Upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DataBase from './Components/DataBase';
-import Add from './Components/Add';
+import DataBase from "./Components/DataBase";
+import Add from "./Components/Add";
 
 function App() {
   return (
-
-
-<BrowserRouter>
-<Routes>
-  <Route path="/" element={<DataBase />} />
-    <Route path="/add" element={<Add />} />
-</Routes>
-</BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DataBase />} />
+        <Route path="/add" element={<Add />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
